d27bdd8c5f43f64cfe0e2588a1820c607fb45c5c
Charlie Vieth <charlie.vieth@gmail.com>
2015-08-15 18:29:45 -0400

    More parse - worked on this too long - stupid

--- parse.go
+++ parse.go
@@ -25,7 +25,9 @@ type context struct {
 	info    *types.Info
 	infoErr error
 
-	af *ast.File
+	af    *ast.File   // Source file
+	files []*ast.File // Package files
+	fset  *token.FileSet
 }
 
 func newContext(filename string, src []byte, ctx *build.Context) (*context, error) {
@@ -52,8 +54,7 @@ func (c *context) checkTypes(dirname string) error {
 	if c.info != nil {
 		return c.infoErr
 	}
-	files, fset, err := c.parseSourceFiles(dirname)
-	if files == nil {
+	if err := c.parseSourcePkg(dirname); err != nil {
 		return err
 	}
 	info := &types.Info{
@@ -61,7 +62,7 @@ func (c *context) checkTypes(dirname string) error {
 		Uses: make(map[*ast.Ident]types.Object),
 	}
 	conf := types.Config{}
-	if _, err := conf.Check(dirname, fset, files, info); err != nil {
+	if _, err := conf.Check(dirname, c.fset, c.files, info); err != nil {
 		// Return error only if missing type info.
 		if len(info.Defs) == 0 || len(info.Uses) == 0 {
 			c.infoErr = err
@@ -72,77 +73,13 @@ func (c *context) checkTypes(dirname string) error {
 	return nil
 }
 
-func (c *context) prsSourceFiles(dirname string) ([]*ast.File, *token.FileSet, error) {
-	names, err := c.PkgFiles(dirname)
-	if err != nil {
-		return nil, nil, err
-	}
-
-	// Read files here to prevent concurrent disk access.
-	var index int
-	srcs := make([][]byte, len(names))
-	for i := 0; i < len(names); i++ {
-		path := filepath.Join(dirname, names[i])
-		names[i] = path
-		if path == c.filename {
-			srcs[i] = c.src
-			index = i
-		} else {
-			if src, _ := ioutil.ReadFile(path); src != nil {
-				srcs[i] = src
-			}
-		}
-	}
-
-	// Parse response
-	type response struct {
-		af  *ast.File
-		err error
-	}
-	reschs := make([]chan response, 0, len(srcs))
-
-	fset := token.NewFileSet()
-	for i, name := range names {
-		ch := make(chan response)
-		reschs = append(reschs, ch)
-		go func(name string, src []byte, ch chan response) {
-			af, err := parser.ParseFile(fset, name, src, parser.ParseComments)
-			ch <- response{af, err}
-		}(name, srcs[i], ch)
-	}
-
-	var first error
-	files := make([]*ast.File, 0, len(srcs))
-	for i, ch := range reschs {
-		switch r := <-ch; {
-		case r.af != nil:
-			files = append(files, r.af)
-			if i == index {
-				c.af = r.af
-			}
-		case r.err != nil:
-			switch {
-			case i == index:
-				first = r.err
-			case first == nil:
-				first = r.err
-			}
-		}
-	}
-	if c.af == nil {
-		return nil, nil, errors.New("error parsing src file: " + first.Error())
-	}
-	return files, fset, first
-}
-
-func (c *context) pSourceFiles(dirname string) ([]*ast.File, *token.FileSet, error) {
+func (c *context) parseSourcePkg(dirname string) error {
 	srcs, err := c.readPkgSource(dirname)
 	if err != nil {
-		return nil, nil, err
+		return err
 	}
 
-	fset := token.NewFileSet()
-	var files []*ast.File
+	c.fset = token.NewFileSet()
 	var first error
 
 	mu := new(sync.Mutex)
@@ -152,11 +89,11 @@ func (c *context) pSourceFiles(dirname string) ([]*ast.File, *token.FileSet, err
 	for name, src := range srcs {
 		go func(name string, src []byte) {
 			defer wg.Done()
-			af, err := parser.ParseFile(fset, name, src, parser.ParseComments)
+			af, err := parser.ParseFile(c.fset, name, src, parser.ParseComments)
 			mu.Lock()
 			switch {
 			case af != nil:
-				files = append(files, af)
+				c.files = append(c.files, af)
 				if name == c.filename {
 					c.af = af
 				}
@@ -173,44 +110,14 @@ func (c *context) pSourceFiles(dirname string) ([]*ast.File, *token.FileSet, err
 	}
 	wg.Wait()
 
-	return files, fset, first
-}
-
-// fileSrc == filepath -> filesrc
-func parseFiles(fileSrc map[string][]byte) ([]*ast.File, *token.FileSet, error) {
-	// Parse response
-	type response struct {
-		af  *ast.File
-		err error
+	if c.af == nil {
+		return first
 	}
-	reschs := make([]chan response, 0, len(fileSrc))
-
-	fset := token.NewFileSet()
-	for name, src := range fileSrc {
-		ch := make(chan response)
-		reschs = append(reschs, ch)
-		go func(name string, src []byte, ch chan response) {
-			af, err := parser.ParseFile(fset, name, src, parser.ParseComments)
-			ch <- response{af, err}
-		}(name, src, ch)
-	}
-
-	var first error
-	files := make([]*ast.File, 0, len(fileSrc))
-	for _, ch := range reschs {
-		switch r := <-ch; {
-		case r.af != nil:
-			files = append(files, r.af)
-		case r.err != nil && first == nil:
-			first = r.err
-		}
-	}
-
-	return files, fset, first
+	return nil
 }
 
 func (c *context) readPkgSource(dirname string) (map[string][]byte, error) {
-	names, err := c.PkgFiles(dirname)
+	names, err := c.pkgFiles(dirname)
 	if err != nil {
 		return nil, err
 	}
@@ -231,31 +138,7 @@ func (c *context) readPkgSource(dirname string) (map[string][]byte, error) {
 	return srcs, nil
 }
 
-func (c *context) parseSourceFiles(dirname string) ([]*ast.File, *token.FileSet, error) {
-	names, err := c.PkgFiles(dirname)
-	if err != nil {
-		return nil, nil, err
-	}
-
-	// Read files here to prevent concurrent disk access.
-	fileSrc := make(map[string][]byte)
-	for _, name := range names {
-		path := filepath.Join(dirname, name)
-		if path == c.filename {
-			fileSrc[path] = c.src
-		} else {
-			if src, _ := ioutil.ReadFile(path); src != nil {
-				fileSrc[path] = src
-			}
-		}
-	}
-	if len(fileSrc) == 0 {
-		return nil, nil, errors.New("error reading files in: " + dirname)
-	}
-	return parseFiles(fileSrc)
-}
-
-func (c *context) PkgFiles(dir string) ([]string, error) {
+func (c *context) pkgFiles(dir string) ([]string, error) {
 	f, err := os.Open(dir)
 	if err != nil {
 		return nil, err
@@ -285,6 +168,19 @@ func (c *context) MatchFile(dir, name string) (match bool) {
 		match, _ = c.ctx.MatchFile(dir, name)
 	}
 	return match
+}
+
+func (c *context) tokenFile() (*token.File, error) {
+	if c.af == nil {
+		return nil, errors.New("nil file")
+	}
+	if c.fset == nil {
+		return nil, errors.New("nil fset")
+	}
+	if !c.af.Pos().IsValid() {
+		return nil, errors.New("invalid file pos")
+	}
+	return c.fset.File(c.af.Pos()), nil
 }
 
 func isGoSource(s string, includeTest bool) bool {
